78006af1f731bf6cd7b893fea87af59d874992ca
ljm0850 <dlwoals0850@gmail.com>
2022-07-19 16:38:35 +0900

    router

--- frontend-new/goose/src/App.vue
+++ frontend-new/goose/src/App.vue
@@ -1,31 +1,7 @@
 <template>
+<div>
   <NavBar/>
-  <div class="dropdown">
-    <button
-      class="btn btn-secondary dropdown-toggle"
-      type="button"
-      id="dropdownMenuButton1"
-      data-bs-toggle="dropdown"
-      aria-expanded="false"
-    >
-      Check Bootstrap
-    </button>
-      <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-        <li><a class="dropdown-item" href="#">Action</a></li>
-        <li><a class="dropdown-item" href="#">Another action</a></li>
-        <li><a class="dropdown-item" href="#">Something else here</a></li>
-      </ul>
-    </div>
-  <div class="dropdown">
-  <a class="btn btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-bs-toggle="dropdown" aria-expanded="false">
-    Dropdown link
-  </a>
-
-  <ul class="dropdown-menu" aria-labelledby="dropdownMenuLink">
-    <li><a class="dropdown-item" href="#">Action</a></li>
-    <li><a class="dropdown-item" href="#">Another action</a></li>
-    <li><a class="dropdown-item" href="#">Something else here</a></li>
-  </ul>
+  <router-view />
 </div>
 </template>
 
@@ -33,7 +9,7 @@
 import NavBar from '@/components/NavBar'
 
 export default {
-  name: "HelloWorld",
+  name: "App",
   
   props: {
     msg: String,
