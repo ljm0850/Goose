3e57cc2ee8f0050908783b1dabcec19b4f6360fc
sjhyun1006 <sjhyun1006@naver.com>
2022-07-19 23:21:17 +0900

    Feat:SignUp.vue

--- frontend-new/goose/src/App.vue
+++ frontend-new/goose/src/App.vue
@@ -1,37 +1,11 @@
 <template>
   <NavBar/>
-  <div class="dropdown">
-    <button
-      class="btn btn-secondary dropdown-toggle"
-      type="button"
-      id="dropdownMenuButton1"
-      data-bs-toggle="dropdown"
-      aria-expanded="false"
-    >
-      Check Bootstrap
-    </button>
-      <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-        <li><a class="dropdown-item" href="#">Action</a></li>
-        <li><a class="dropdown-item" href="#">Another action</a></li>
-        <li><a class="dropdown-item" href="#">Something else here</a></li>
-      </ul>
-    </div>
-  <div class="dropdown">
-  <a class="btn btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-bs-toggle="dropdown" aria-expanded="false">
-    Dropdown link
-  </a>
-
-  <ul class="dropdown-menu" aria-labelledby="dropdownMenuLink">
-    <li><a class="dropdown-item" href="#">Action</a></li>
-    <li><a class="dropdown-item" href="#">Another action</a></li>
-    <li><a class="dropdown-item" href="#">Something else here</a></li>
-  </ul>
-</div>
+  <SignUp/>
 </template>
 
 <script>
 import NavBar from '@/components/NavBar'
-
+import SignUp from '@/views/accounts/SignUpView'
 export default {
   name: "HelloWorld",
   
@@ -41,6 +15,7 @@ export default {
 
   components:{
     NavBar,
+    SignUp,
   }
 };
 </script>
