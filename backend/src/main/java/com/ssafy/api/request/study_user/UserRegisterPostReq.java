88b29c464de09a8035c5a148fce0cadb73462d04
이승진 <pelor7317@naver.com>
2022-07-25 17:16:02 +0900

    fix:merge error

--- backend/src/main/java/com/ssafy/api/request/study_user/UserRegisterPostReq.java
+++ backend/src/main/java/com/ssafy/api/request/study_user/UserRegisterPostReq.java
@@ -12,23 +12,6 @@ import lombok.Setter;
 @Setter
 @ApiModel("UserRegisterPostRequest")
 public class UserRegisterPostReq {
-<<<<<<< HEAD
-	@ApiModelProperty(name="유저 ID", example="ssafy_web")
-	String id;
-	@ApiModelProperty(name="유저 Password", example="your_password")
-	String password;
-	@ApiModelProperty(name="유저 Name", example="your_name")
-	String name;
-	@ApiModelProperty(name="유저 가입일", example="your_joinDate")
-	String joinDate;
-	@ApiModelProperty(name="유저 Email", example="your_email")
-	String email;
-	@ApiModelProperty(name="유저 프로필 사진", example="your_photo")
-	String photo;
-	@ApiModelProperty(name="유저 Authority", example="your_authority")
-	int authority;
-	@ApiModelProperty(name="유저 Interest", example="your_interest")
-=======
 	@ApiModelProperty(name="유저 ID", example="(String) 로그인 아이디")
 	String id;
 	@ApiModelProperty(name="유저 Password", example="(String) 패스워드")
@@ -40,6 +23,5 @@ public class UserRegisterPostReq {
 	@ApiModelProperty(name="유저 프로필 사진", example="(String) 사진 사용X")
 	String photo;
 	@ApiModelProperty(name="유저 Interest", example="(String) 관심")
->>>>>>> 2e09ea8d (feat: 스터디 게시판 + 댓글)
 	String interest;
 }
