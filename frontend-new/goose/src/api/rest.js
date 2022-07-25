58983daf3ba5d76bd4b88676466f26ddaa7c0506
sjhyun1006 <sjhyun1006@naver.com>
2022-07-25 17:09:45 +0900

    FE-fix

--- frontend-new/goose/src/api/rest.js
+++ frontend-new/goose/src/api/rest.js
@@ -1,15 +1,11 @@
 const HOST = 'http://localhost:8080/api/v1/'
 
 const Article = 'articles/'
-<<<<<<< HEAD
 const ArticleReply = 'articlesReply/'
-=======
->>>>>>> 2e09ea8d (feat: 스터디 게시판 + 댓글)
 const Auth = 'auth/login'
 const Study = 'study/'
 const User = 'users'
 
-<<<<<<< HEAD
 
 
 export default{
@@ -26,14 +22,6 @@ export default{
         reply_cr: () => HOST + ArticleReply,
         reply_ud: id => HOST + ArticleReply + `${id}`,
 
-=======
-export default{
-    auth_login:{
-    login: () => HOST + Auth
-    },
-    article: {
-    article_create: () => HOST + Article
->>>>>>> 2e09ea8d (feat: 스터디 게시판 + 댓글)
     },
     study: {
         study_create: () => HOST + Study + 'create',
