45ace1bc5ad099893ef80eb9d9d5911c7ef5d5a0
leejaemin <dlwoals0850@gmail.com>
2022-07-25 15:58:12 +0900

    FE ljm backup

--- frontend-new/goose/src/views/StudyPage/createStudy.vue
+++ frontend-new/goose/src/views/StudyPage/createStudy.vue
@@ -1,47 +1,75 @@
 <template>
-<h3>스터디 만들기</h3>
-<form>
-<select v-model="state.credential.open" class="form-select" aria-label="Default select example">
-  <option selected>공개 여부</option>
-  <option value="0">공개</option>
-  <option value="1">비공개</option>
-</select>
-<select v-model="state.credential.maxMember" class="form-select" aria-label="Default select example">
-  <option selected>총원</option>
-  <option value="1">1명</option>
-  <option value="2">2명</option>
-  <option value="3">3명</option>
-  <option value="4">4명</option>
-  <option value="5">5명</option>
-  <option value="6">6명</option>
-</select>
-<select v-model="state.credential.category" class="form-select" aria-label="Default select example">
-  <option selected>분류</option>
-  <option value="1">알고리즘</option>
-  <option value="2">토익</option>
-  <option value="3">칵테일</option>
-  <option value="3">면접</option>
-</select>
-<div class="form-floating">
-  <textarea v-model="state.credential.title" class="form-control" placeholder="Leave a comment here" id="floatingTextarea"></textarea>
-  <label for="floatingTextarea">스터디 이름</label>
+<div class="container">
+<div class="d-flex justify-content-center m-5">
+  <h3>스터디 만들기</h3>
 </div>
+<div class="container">
+<form>
+  <!-- 공개스터디, 비공개 스터디 -->
+  <div class="d-flex">
+    <p class="text-capitalize m-3">공개 여부</p>
+    <div class="m-3">
+      <select v-model="state.credential.open" class="form-select" aria-label="Default select example">
+        <option value="0">공개</option>
+        <option value="1">비공개</option>
+      </select>
+    </div>
+  </div>
+<!-- 최대인원 -->
+  <div class="d-flex">
+    <p class="text-capitalize m-3">총원</p>
+    <div class="m-3">
+      <select v-model="state.credential.maxMember" class="form-select" aria-label="Default select example">
+        <option value="1">1명</option>
+        <option value="2">2명</option>
+        <option value="3">3명</option>
+        <option value="4">4명</option>
+        <option value="5">5명</option>
+        <option value="6">6명</option>
+    </select>
+    </div>
+  </div>
+
+  <!-- 카테고리 -->
+  <div class="d-flex container">
+    <div>카테고리</div>
+    <select v-model="state.credential.category" class="form-select" aria-label="Default select example">
+      <option selected>분류</option>
+      <option value="1">알고리즘</option>
+      <option value="2">토익</option>
+      <option value="3">칵테일</option>
+      <option value="3">면접</option>
+    </select>
+  </div>
+  <!-- 스터디 이름 -->
+  <div>
+
+    <div class="form-floating">
+      <textarea v-model="state.credential.title" class="form-control" placeholder="Leave a comment here" id="floatingTextarea"></textarea>
+      <label for="floatingTextarea">스터디 이름</label>
+    </div>
+  </div>
 <!-- <div class="form-floating">
   <textarea v-model="" class="form-control" placeholder="Leave a comment here" id="floatingTextarea"></textarea>
   <label for="floatingTextarea">스터디 소개</label>
 </div> -->
+
+<!-- 비공개방일시 비밀번호 -->
 <div class="form-floating">
   <input v-model="state.credential.password" type="password" class="form-control" id="floatingPassword" placeholder="Password">
   <label for="floatingPassword">Password</label>
 </div>
+
+<!-- 사진 url -->
 <div class="form-floating">
   <textarea v-model="state.credential.image" class="form-control" placeholder="Leave a comment here" id="floatingTextarea"></textarea>
   <label for="floatingTextarea">대표 사진 url</label>
 </div>
   <button @click.prevent="createStudy(state.credential)" type="submit" class="btn btn-primary">Submit</button>
 </form>
-
+</div>
 {{ state.credential }}
+</div>
 </template>
 
 <script>
