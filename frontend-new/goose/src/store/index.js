8ed9b20e86d3c5b488c354f08eddad68868f94d7
Ingyeon <qwe0274@naver.com>
2022-07-28 17:49:40 +0900

    feat: persistedstate 적용

--- frontend-new/goose/src/store/index.js
+++ frontend-new/goose/src/store/index.js
@@ -14,7 +14,9 @@ import { createStore } from "vuex";
 import accounts from './accounts.js'
 import study_page from './study_page.js'
 import articles from './articles.js'
+import createPersistedState from 'vuex-persistedstate';
 
 export default createStore({
-  modules: { accounts,study_page,articles }
+  modules: { accounts,study_page,articles },
+  plugins: [createPersistedState()]
 });
