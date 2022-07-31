4074aaed3510cfdb8d8ea76e6ef1831023e11476
sjhyun1006 <sjhyun1006@naver.com>
2022-07-31 16:10:36 +0900

    sytle:signup

--- frontend-new/goose/src/views/accounts/signupView.vue
+++ frontend-new/goose/src/views/accounts/signupView.vue
@@ -1,16 +1,16 @@
 <template>
     <div class="container mt-5">
         <form @submit.prevent="signupForm">
-            <div class="inputBox">
+            <div class="input-Box">
                 <label for="inputname" class="form-label">이름</label>
                 <input type="text" id="inputname" class="form-control" placeholder="이름" v-model="state.form.name">
             </div>
-            <div class="inputBox">
+            <div class="input-Box">
                 <label for="inputId" class="form-label">ID</label>
                 <input type="text" id="inputId" class="form-control" placeholder="ID를 입력해주세요" v-model="state.form.id" maxlength=16 @blur="idValid">
-                <div v-if="!state.idValidFlag">유효하지 않은 아이디 입니다. 영문과 숫자를 조합하여 8-16자 이내로 만드세요.</div>
+                <div v-if="!state.idValidFlag" class="form-text">유효하지 않은 아이디 입니다. 영문과 숫자를 조합하여 8-16자 이내로 만드세요.</div>
             </div>
-            <div class="inputBox">
+            <div class="input-Box">
                 <label for="inputPassword" class="form-label">Password</label>
                 <input type="password" id="inputPassword" class="form-control" placeholder="비밀번호를 입력해주세요" aria-describedby="passwordHelpBlock" v-model="state.form.password1" @blur="passwordValid">
                 <div id="passwordHelpBlock" class="form-text">
@@ -22,18 +22,18 @@
                 <div v-if="!state.passwordCheckFlag" class="form-text">비밀번호가 동일하지 않습니다.</div>
 
             </div>
-             <div class="inputBox">
+             <div class="input-Box">
                 <label for="inputEmail" class="form-label">Email</label>
                 <input type="email" class="form-control" id="inputEmail" v-model="state.form.email" @blur="emailValid">
                 <div v-if="!state.emailValidFlag" class="form-text">이메일을 제대로 입력해주세요.</div>
             </div>
-            <div class="inputBox">
+            <div class="input-Box">
                 <label for="inputInterest" class="form-label">Interest</label>
                 <input type="text" id="inputInterest" class="form-control" placeholder="취미를 입력해주세요" v-model="state.form.interest">
             </div>
-            <div class="inputBox">
+            <div class="input-Box">
               <input type="submit" value="Submit">  
-              <router-link to="/login" class="btn input-box-btn">Back</router-link>
+              <!-- <router-link to="/login" class="btn input-box-btn">Back</router-link> -->
             </div>
         </form>  
     </div>
@@ -137,31 +137,41 @@ export default {
 </script>
 
 <style>
-    .inputBox {
+    @import url(https://cdn.jsdelivr.net/gh/moonspam/NanumSquare@1.0/nanumsquare.css);
+    
+    .input-Box {
     width: 100%;
     margin-top: 20px;
     }
-
-    .inputBox:nth-child(5) {
+    
+    .input-Box:nth-child(6) {
     display: flex;
-    justify-content: space-evenly;
+    /* justify-content: space-evenly; */
+    justify-content: end;
+
     }
 
-    .inputBox input::placeholder {
+    .input-Box input::placeholder {
     color: #666
     }
 
-    .input-box-btn {
-    background: #fff;
-    color: #666;
-    cursor: pointer;
-    width: 100px;
-    margin-bottom: 20px;
-    font-weight: 600;
-    text-align: center;
+    .input-Box label[class="form-label"] {
+        font-family: 'NanumSquare', sans-serif;
+        font-weight: bold;
+        font-size: 18px;
     }
-    
-    .inputBox input[type="submit"] {
+
+    .input-Box input[class="form-control"] {
+        width: 100%;
+        height: 50px;
+        margin-top: 3px;
+        font-family: 'NanumSquare', sans-serif;
+    }
+
+    .input-Box div[class="form-text"] {
+        font-family: 'NanumSquare', sans-serif;
+    }
+    /* .input-box-btn {
     background: #fff;
     color: #666;
     cursor: pointer;
@@ -169,6 +179,17 @@ export default {
     margin-bottom: 20px;
     font-weight: 600;
     text-align: center;
+    } */
+    
+    .input-Box input[type="submit"] {
+    background: #ffd700;
+    color: #000000;
+    cursor: pointer;
+    width: 100px;
+    margin-bottom: 20px;
+    font-weight: 600;
+    text-align: center;
+    border-radius: 40px 80px;
     }
 
 </style>
