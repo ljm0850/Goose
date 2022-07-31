7b65b3ab7aeb813abe939f882b60e1ac97dc0f37
sjhyun1006 <sjhyun1006@naver.com>
2022-07-31 17:03:15 +0900

    style-fix:main.css

--- frontend-new/goose/src/views/accounts/loginView.vue
+++ frontend-new/goose/src/views/accounts/loginView.vue
@@ -1,23 +1,48 @@
 <template>
 <h1>로그인</h1>
-<form>
-  <div class="mb-3">
-    <label for="IdInput" class="form-label">Id</label>
-    <input type="text" class="form-control" id="IdInput" v-model="state.form.id">
-  </div>
-  <div class="mb-3">
-    <label for="passwordInput" class="form-label">Password</label>
-    <input type="password" class="form-control" id="passwordInput" v-model="state.form.password">
-  </div>
-  <button type="submit" @click.prevent="clickLogin" class="btn btn-primary">로그인</button>
-  <router-link to="/signup" class="btn btn-secondary">회원가입</router-link>
-</form>
-
+<div class="container">
+  <form @submit.prevent="clickLogin">
+    <div class="input-Box">
+      <label for="IdInput" class="form-label">ID</label>
+      <input type="text" class="form-control" id="IdInput" v-model="state.form.id">
+    </div>
+    <div class="input-Box">
+      <label for="passwordInput" class="form-label">Password</label>
+      <input type="password" class="form-control" id="passwordInput" v-model="state.form.password">
+    </div>
+    <div class="input-Box">
+      <button @click.prevent="clickSignup">회원가입</button>
+      <input type="submit" value="로그인">
+      <!-- <router-link to="/signup" class="btn">회원가입</router-link> -->
+    </div>
+  </form>
+</div>
 </template>
+
+<style>
+  
+  .input-Box:nth-child(3) {
+    display: flex;
+    /* justify-content: space-evenly; */
+    justify-content: end;
+
+    }
+    .input-Box button{
+    background: #ffd700;
+    color: #000000;
+    cursor: pointer;
+    width: 100px;
+    margin-bottom: 20px;
+    font-weight: 600;
+    text-align: center;
+    border-radius: 80px 40px;
+  }
+</style>
 
 <script>
 import {reactive,computed} from 'vue'
 import {useStore} from 'vuex'
+import { useRouter } from 'vue-router'
 
 export default {
     name: 'loginView',
@@ -25,6 +50,7 @@ export default {
     setup(){
     const store = useStore()
     const token = computed(() => store.getters.isLoggedIn)
+    const router = useRouter()
 
     const state = reactive({
         form:{
@@ -38,7 +64,9 @@ export default {
     //     alert(JSON.stringify(state.form))
 
     // }
-
+    const clickSignup = function(){
+      router.push({name:"Signup"})
+    }
     const clickLogin = function(){
         // dispatch 함수명 바뀔 경우 아래줄 수정
         console.log(state.form.id, state.form.password)
@@ -50,7 +78,7 @@ export default {
             alert(err)
         })
       }
-    return {state,clickLogin,store,token}
+    return {router, state,clickLogin,store,token, clickSignup}
     }
 }
 </script>
