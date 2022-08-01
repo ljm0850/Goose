63f7bf365978567e8c4ada08e989224645fbf187
Ingyeon <qwe0274@naver.com>
2022-08-01 21:53:56 +0900

    feat: 데이터 복구 및 css, 자잘한 버그 수정

--- frontend-new/goose/src/views/article/newArticle.vue
+++ frontend-new/goose/src/views/article/newArticle.vue
@@ -1,10 +1,10 @@
 <template>
-<h1>스터디 참여하기</h1>
-<div>
+<h1 class="d-flex my-2 justify-content-center">스터디 참여하기</h1>
+<div class="mb-3 container">
 <form>
-        <label class="form-label">인원 선택</label>
+        <label class="form-label my-1">인원 선택</label>
     <select class="form-select" v-model="state.form.recruitment">
-  <option selected>인원</option>
+  <option value=null selected>인원</option>
   <option value=1>1</option>
   <option value=2>2</option>
   <option value=3>3</option>
@@ -13,45 +13,41 @@
     <option value=6>6</option>
 </select>
 
-      <label class="form-label">모집할 내 스터디 선택</label>
+      <label class="form-label my-1">모집할 내 스터디 선택</label>
     <select class="form-select" v-model="state.form.study_pk">
-  <option selected>선택</option>
-  <!-- 함수로 for문 돌려서 셀렉트 박스에 넣기 -->
-  
-  <option v-for="item in state.my_study"  :value="item.id">{{  item.title }}</option> 
-<!-- 게시글 작성자가 운영중인 스터디의 이름들을 목록에 뿌려줘야함 -->
+  <option v-for="item in study_list"  :value="item.id">{{  item.title }}</option> 
+
 
 </select>
 
-    <label class="form-label">분류 선택</label>
+    <label class="form-label my-1">분류 선택</label>
     <select class="form-select" v-model="state.form.category">
-  <option selected>선택</option>
+  <option value=null selected>선택</option>
   <option value="면접">면접</option>
   <option value="토익">토익</option>
   <option value="알고리즘">알고리즘</option>
     <option value="개인공부">개인공부</option>
     <option value="기타">기타</option>
 </select>
-  <div class="mb-3">
+  <div class="mb-3 input-Box">
     <label for="titleInput" class="form-label">제목</label>
-    <input type="text" class="form-control" id="titleInput" v-model="state.form.title">
+    <input type="text" class="form-control" id="titleInput" v-model="state.form.title" required>
   </div>
-  <div class="mb-3">
+  <div class="mb-3 input-Box">
     <label for="textarea" class="form-label">내용</label>
-    <textarea type="textarea" class="form-control" id="textarea" v-model="state.form.content"></textarea>
+    <textarea type="textarea" class="form-control" id="textarea" v-model="state.form.content" required></textarea>
   </div>
+  <div class="input-Box">
   <button type="submit" @click.prevent="clickSet" class="btn btn-primary">작성</button>
-    
+    </div>
 </form>
-<button @click="test">123</button>
+{{state.form}}
 </div>
 
-<!-- 확인용 -->
-{{state.form}}
 </template>
 
 <script>
-import {onMounted, reactive} from 'vue'
+import {computed, onMounted, reactive} from 'vue'
 import {useStore} from 'vuex'
 import {  useRouter  } from 'vue-router'
 
@@ -63,10 +59,10 @@ export default {
     const store = useStore()
     const state = reactive({
         form:{
-            recruitment: '',
-            category: '',
-            title: '',
-            content: '',
+            recruitment: null,
+            category: null,
+            title: null,
+            content: null,
             state: '모집중',
             study_pk: null,
         },
@@ -96,16 +92,32 @@ export default {
         study_pk: state.form.study_pk,
         user_pk: store.getters.loginUser.id,
         })
-        console.log(store.getters.article)
-        // Router.push({path:`/article/${store.getters.Article.id}`})
-
+        Router.push({path:`/article/${store.getters.article.id}`})
     }
-          const test = function(){
-        console.log(store.getters.article)
-      }
     onMounted(() =>study_info())
-
-    return {state,clickSet,store,study_info,test}
+    const study_list = computed(() => store.getters.authStudyList)
+    return {state,clickSet,store,study_info,study_list}
     }
 }
 </script>
+
+<style scoped>
+  .input-Box:nth-child(3) {
+    display: flex;
+    /* justify-content: space-evenly; */
+    justify-content: end;
+
+    }
+    .input-Box button{
+    background: #ffd700;
+    color: #000000;
+    cursor: pointer;
+    width: 100px;
+    margin-bottom: 20px;
+    font-weight: 600;
+    text-align: center;
+    border-radius: 80px 40px;
+    margin-right: 30px;
+  }
+
+</style>
