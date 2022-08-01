ad097249121cba864c38a08c830a5633ebf1b6cc
Ingyeon <qwe0274@naver.com>
2022-08-01 14:18:03 +0900

    feat: 백 적용

--- frontend-new/goose/src/views/article/replyForm.vue
+++ frontend-new/goose/src/views/article/replyForm.vue
@@ -2,7 +2,7 @@
 <div>
     <b-input-group>
     <b-input-group-text>댓글</b-input-group-text>
-    <b-form-textarea @submit.prevent="onSubmit" id="textarea" v-model="re_content" placeholder="댓글을 입력해주세요." required></b-form-textarea>
+    <b-form-textarea @submit.prevent="onSubmit" id="textarea" v-model="state.form.re_content" placeholder="댓글을 입력해주세요." required></b-form-textarea>
     <b-input-group-append>
         <b-button variant="outline-secondary" @click="onSubmit">작성</b-button>
     </b-input-group-append>
@@ -28,9 +28,13 @@ export default {
         })
 
         const onSubmit = function(){
+            if (state.form.re_content !== ''){
             store.dispatch('createReply',{article_pk: state.article.id, re_content: state.form.re_content})
             state.form.re_content = ''
-            router.go()
+            router.go()}
+            else if (state.form.re_content == ''){
+                alert('내용을 입력하세요.')
+            }
         }
 
     return {onSubmit,store,state}}}
