559afcb469f307f03a0a6bd508b194e885a4c388
kc <kc713@naver.com>
2022-08-01 14:11:42 +0900

    fix: delete header

--- backend/src/main/java/com/ssafy/api/controller/ArticleReplyController.java
+++ backend/src/main/java/com/ssafy/api/controller/ArticleReplyController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -118,7 +119,7 @@ public class ArticleReplyController {
         @ApiResponse(code = 204, message = "성공"),
     })
 	public ResponseEntity<? extends BaseResponseBody> deleteArticleReply(
-			@RequestBody @ApiParam(value="id", required = true) Long id, @ApiIgnore Authentication authentication) {
+			@RequestHeader @ApiParam(value="id", required = true) Long id, @ApiIgnore Authentication authentication) {
 		SsafyUserDetails userDetails = (SsafyUserDetails) authentication.getDetails();
 		articleReplyService.deleteArticleReplyById(id);
 		
