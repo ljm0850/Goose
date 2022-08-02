44dd8bde6e86d693a63b3b3cb49501feaad605fb
leejaemin <dlwoals0850@gmail.com>
2022-08-02 20:22:19 +0900

    FE store bug fix

--- frontend-new/goose/src/store/index.js
+++ frontend-new/goose/src/store/index.js
@@ -14,7 +14,7 @@ import accounts from "./accounts.js";
 import study_page from "./study_page.js";
 import articles from "./articles.js";
 import createPersistedState from "vuex-persistedstate";
-import study_article from "./study_article"
+import study_article from "./study_Article.js"
 export default createStore({
   modules: { accounts, study_page, articles, study_article },
   plugins: [createPersistedState()],
