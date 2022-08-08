34b81110926585d2625c3017d8f984c892634d99
Ingyeon <qwe0274@naver.com>
2022-08-08 17:39:32 +0900

    feat: 댓글 페이지네이션

--- backend/src/main/java/com/ssafy/api/response/study/StudyMyList.java
+++
@@ -1,9 +0,0 @@
-package com.ssafy.api.response.study;
-
-public interface StudyMyList {
-	Long getId();
-	String getTitle();
-	String getImage();
-	String getCategory();
-
-}
--- backend/src/main/java/com/ssafy/api/service/FileService.java
+++
@@ -1,8 +0,0 @@
-package com.ssafy.api.service;
-
-import com.ssafy.api.response.article.FileDto;
-
-public interface FileService {
-	public Long saveFile(FileDto fileDto);
-	public FileDto getFile(Long id);
-}
