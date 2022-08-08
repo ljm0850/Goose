206ea6eb616e9ca10984cd11e8ddf322390d31d0
sjhyun1006 <sjhyun1006@naver.com>
2022-08-08 10:53:03 +0900

    style:home

--- frontend-new/goose/src/views/home/component/articleItem.vue
+++ frontend-new/goose/src/views/home/component/articleItem.vue
@@ -5,7 +5,7 @@
           <img :src="state.photo" class="card-img-top" alt="alt">
           <div class="card-body">
             <h5 class="card-title fw-bold">{{item.title}}</h5>
-            <p class="card-text">사용 언어: {{}}</p>
+            <p class="card-text">사용 언어: {{item.category}}</p>
             <div class="d-flex justify-content-end">
               <button @click="clickbtn(item.id)" class="button" >스터디 입장하기</button>
             </div>
@@ -33,7 +33,7 @@ export default {
 
   setup(props){
     const store = useStore()
-    const myStudyList = computed(()=> store.getters.myStudyList)
+    // const myStudyList = computed(()=> store.getters.myStudyList)
     const router = useRouter();
     // const openstudyList = computed(()=> store.getters.openstudyList)
     const openstudyList = store.getters.openstudyList
@@ -42,9 +42,9 @@ export default {
       photo : ""
     })
     
-    const fetchMyStudyList = function(studyId){
-      store.dispatch('myStudyList',studyId)
-    }
+    // const fetchMyStudyList = function(studyId){
+    //   store.dispatch('myStudyList',studyId)
+    // }
     
     const changePhoto = ()=>{
       if (props.item.image==='study1'){state.photo = study1}
@@ -53,9 +53,10 @@ export default {
     }
     changePhoto()
     const open_set = function(){
-      console.log('시작')
-      console.log(openstudyList)
+      // console.log('시작')
+      // console.log(openstudyList)
       store.dispatch('saveOpenList')
+      // console.log(openstudyList)
     }
     
     const fetchStudyHome = async function (studyId) {
@@ -71,14 +72,14 @@ export default {
     // open_set()
 
     // const joinStudy = (studyId) => store.dispatch('joinStudy',studyId)
-    return {myStudyList, router, store, fetchMyStudyList,openstudyList,open_set, changePhoto,state,clickbtn}
+    return { router, store,openstudyList,open_set, changePhoto,state,clickbtn}
   },
 
   watch: {
     $route: {
       handler() {
         // this.open_set()
-        this.fetchMyStudyList()
+        // this.fetchMyStudyList()
         this.open_set()
       },
       immediate: true
