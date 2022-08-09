43ecf8407085e65ebc5f7b4404500d71314fc490
정관철 <kc713@naver.com>
2022-08-09 13:57:52 +0900

    fix: merge conflicts

--- frontend-new/goose/src/components/StudyPage/callender.vue
+++ frontend-new/goose/src/components/StudyPage/callender.vue
@@ -1,49 +1,22 @@
 <template>
-<div class="container">
-  <div class="d-flex justify-content-between">
-    <h4>스터디 일정관리</h4>
-    <button type="button" class="btn btn-3 hover-border-3" data-bs-toggle="modal" data-bs-target="#callenderModal">
-        전체보기
-    </button>
-  </div>
-  <!-- 전체보기 누를경우 보이는 내용 -->
-  <div class="modal fade" id="callenderModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-fullscreen modal-dialog">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h5 class="modal-title" id="exampleModalLabel">캘린더</h5>
-          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-        </div>
-        <div class="modal-body">
-          <div>      <FullCalendar :options="calendarOptionsM" /></div>
-    
-        </div>
-        <div class="modal-footer">
-           
-          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">
-            Close
-          </button>
-        </div>
-      </div>
+  <div class="container">
+    <div class="d-flex justify-content-between">
+      <h4>스터디 일정관리</h4>
+    </div>
+    <br />
+
+    <div class="container box">
+      <FullCalendar id="calendar" :options="calendarOptionsM2" />
     </div>
   </div>
-  
-  <br>
-  <div class="container box">
-    <FullCalendar id="qwe" :options="calendarOptionsW" />
+  <div id="app">
+    <CreateCalendar
+      v-show="isCModalVisible"
+      :time="time"
+      @close="closeCModal"
+    />
+    <DetailCalendar v-show="isDModalVisible" @close="closeDModal" />
   </div>
-  <!-- <div v-if="createModal" >
-    <div >    
-      <h4>상세페이지</h4>
-      <p>상세 페이지 내용</p>
-      <button>닫기</button>
-    </div>
-  </div> -->
-</div>
-    <div id="app">
-      <button type="button" class="btn" @click="showModal">Open Modal!</button>
-      <HelloWorld v-show="isModalVisible" @close="closeModal"/>
-    </div>
 </template>
 
 <script>
@@ -53,45 +26,48 @@ import dayGridPlugin from "@fullcalendar/daygrid";
 import interactionPlugin from "@fullcalendar/interaction";
 import koLocale from "@fullcalendar/core/locales/ko";
 import { useStore } from "vuex";
-import HelloWorld from "@/components/StudyPage/HelloWorld.vue"
+import CreateCalendar from "@/components/StudyPage/createCalendar.vue";
+import DetailCalendar from "@/components/StudyPage/detailCalendar.vue";
 
 export default {
-    name: "App",
+  name: "App",
   components: {
     FullCalendar, // make the <FullCalendar> tag available
-      HelloWorld: HelloWorld,
+    CreateCalendar,
+    DetailCalendar,
   },
+
   data() {
     const store = useStore();
-  
+
     return {
-      createModal:true,
-         isModalVisible: false,
+      time: "",
+      isCModalVisible: false,
+      isDModalVisible: false,
       calendars: [],
-      calendarOptionsW: {
-        plugins: [dayGridPlugin, interactionPlugin],
-        // initialView: "dayGridWeek",
-        initialView: "dayGrid", // the name of a generic view
-        duration: { weeks: 4 },
-        selectable: true,
-        dateClick: this.handleDateClick,
-        eventClick: this.handleEventClick,
-        height: 750,
-        locale: koLocale,
-        timeZone: "local",
-        background:"black",
-        background_color: "#FF9900",
-               color:"black",
-        events: JSON.parse(JSON.stringify(store.getters.events)),
-      },
+      // calendarOptionsW: {
+      //   plugins: [dayGridPlugin, interactionPlugin],
+      //   // initialView: "dayGridWeek",
+      //   initialView: "dayGrid", // the name of a generic view
+      //   duration: { weeks: 4 },
+      //   selectable: true,
+      //   dateClick: this.handleDateClick,
+      //   eventClick: this.handleEventClick,
+      //   height: 750,
+      //   locale: koLocale,
+      //   timeZone: "local",
+      //   background: "black",
+      //   background_color: "#FF9900",
+      //   color: "black",
+      //   events: JSON.parse(JSON.stringify(store.getters.events)),
+      // },
       calendarOptionsM: {
         plugins: [dayGridPlugin, interactionPlugin],
         initialView: "dayGridMonth",
         selectable: true,
-        height: 700,
+        height: 750,
         dateClick: this.handleDateClick,
         eventClick: this.handleEventClick,
-
         locale: koLocale,
         timeZone: "local",
         events: JSON.parse(JSON.stringify(store.getters.events)),
@@ -101,33 +77,61 @@ export default {
   methods: {
     handleDateClick: function (arg) {
       alert("date click! " + arg.dateStr);
-      this.createModal=!this.createModal;
-            this.isModalVisible = true;
+      this.time = arg.dateStr + "T00:00";
+      console.log(this.time);
+      this.isCModalVisible = true;
     },
     handleEventClick: function (arg) {
       console.log(arg.event);
       alert("event click! " + arg.event.startStr + arg.event.id);
+      this.$store.dispatch("selectEvent", arg.event.id);
+      this.isDModalVisible = true;
     },
-        showModal() {
-      this.isModalVisible = true;
+    // showCModal() {
+    //   this.isCModalVisible = true;
+    // },
+    closeCModal() {
+      this.isCModalVisible = false;
     },
-    closeModal() {
-      this.isModalVisible = false;
-    }
+
+    // showDModal() {
+    //   this.isDModalVisible = true;
+    // },
+    closeDModal() {
+      this.isDModalVisible = false;
+    },
+  },
+  computed: {
+    calendarOptionsM2: function () {
+      const store = useStore();
+      return {
+        plugins: [dayGridPlugin, interactionPlugin],
+        initialView: "dayGridMonth",
+        selectable: true,
+        height: 750,
+        dateClick: this.handleDateClick,
+        eventClick: this.handleEventClick,
+        locale: koLocale,
+        timeZone: "local",
+        events: JSON.parse(JSON.stringify(store.getters.events)),
+      };
+    },
   },
 };
 </script>
 
 <style scoped>
 h4 {
-    font-weight:bold;
+  font-weight: bold;
 }
 * {
-    font-family: "NanumSquare", sans-serif;
+  font-family: "NanumSquare", sans-serif;
 }
-:active, :hover, :focus {
-outline: 0!important;
-outline-offset: 0;
+:active,
+:hover,
+:focus {
+  outline: 0 !important;
+  outline-offset: 0;
 }
 ::before,
 ::after {
@@ -137,56 +141,62 @@ outline-offset: 0;
 .btn {
   position: relative;
   display: inline-block;
-  width: auto; height: auto;
+  width: auto;
+  height: auto;
   background-color: transparent;
   border: none;
   cursor: pointer;
   margin: 0px 25px 15px;
   min-width: 150px;
 }
-  .btn span {         
-    position: relative;
-    display: inline-block;
-    font-size: 14px;
-    font-weight: bold;
-    letter-spacing: 2px;
-    text-transform: uppercase;
-    top: 0; left: 0;
-    width: 100%;
-    padding: 15px 20px;
-    transition: 0.3s;
-  }
+.btn span {
+  position: relative;
+  display: inline-block;
+  font-size: 14px;
+  font-weight: bold;
+  letter-spacing: 2px;
+  text-transform: uppercase;
+  top: 0;
+  left: 0;
+  width: 100%;
+  padding: 15px 20px;
+  transition: 0.3s;
+}
 .btn.hover-border-3::before,
 .btn.hover-border-3::after {
-  width: 0%; height: 0%;
+  width: 0%;
+  height: 0%;
   opacity: 0;
   transition: width 0.2s 0.15s linear, height 0.15s linear, opacity 0s 0.35s;
 }
 .btn.hover-border-3::before {
-  top: 0; right: 0;
+  top: 0;
+  right: 0;
   border-top: 1px solid rgb(28, 31, 30);
   border-left: 1px solid rgb(28, 31, 30);
 }
 .btn.hover-border-3::after {
-  bottom: 0; left: 0;
+  bottom: 0;
+  left: 0;
   border-bottom: 1px solid rgb(28, 31, 30);
   border-right: 1px solid rgb(28, 31, 30);
 }
 .btn.hover-border-3:hover::before,
 .btn.hover-border-3:hover::after {
-  width: 100%; height: 99%;
+  width: 100%;
+  height: 99%;
   opacity: 1;
-  transition: width 0.2s linear, height 0.15s 0.2s linear, opacity 0s;   
+  transition: width 0.2s linear, height 0.15s 0.2s linear, opacity 0s;
 }
 
-.container.box{
-    background-color: #f0f0f1;
-    margin-top: 1rem;
-    padding-top: 1rem;
-    height:85%;
-    border-radius: 10px;
-    }
-    #qwe{
-      background-color: black;
-    }
+.container.box {
+  background-color: #f0f0f1;
+  margin-top: 1rem;
+  padding-top: 1rem;
+  height: 85%;
+  border-radius: 10px;
+}
+#calendar {
+  background-color: white;
+}
 </style>
--- frontend-new/goose/src/store/study_page.js
+++ frontend-new/goose/src/store/study_page.js
@@ -23,7 +23,7 @@ export default {
     },
     studyBoard: [],
     studyMemberList: [],
-    isStudyMember:false,  // 매니저 찾을때 같이 확인
+    isStudyMember: false, // 매니저 찾을때 같이 확인
     choiceImg: "",
     events: [], // 전체 캘린더
     event: [], // 개별 캘린더
@@ -34,13 +34,13 @@ export default {
       // name:""
     },
     reloadCheck: false,
-    openstudyList: []
+    openstudyList: [],
   },
 
   getters: {
     selectedStudy: (state) => state.selectedStudy,
     myStudyList: (state) => state.myStudyList,
-    isStudyMember: (state)=> state.isStudyMember,
+    isStudyMember: (state) => state.isStudyMember,
     authStudyList: (state) => state.authStudyList, // 권한 가지는 스터디들의 목록
     studyBoard: (state) => state.studyBoard,
     studyJoinList: (state) => state.studyJoinList,
@@ -55,7 +55,7 @@ export default {
     studyManager: (state) => state.studyManager,
     isStudyManager: (state, getters) =>
       state.studyManager.id == getters.loginUser.id,
-    openstudyList: (state) => state.openstudyList ,
+    openstudyList: (state) => state.openstudyList,
   },
 
   mutations: {
@@ -75,8 +75,10 @@ export default {
     SET_RESULT: (state, result) => (state.result = result),
     SET_STUDY_MANAGER: (state, manager) => (state.studyManager = manager),
     SET_RELOADCHECK: (state, reloadCheck) => (state.reloadCheck = reloadCheck),
-    SET_OPENSTUDY_LIST: (state, openstudyList) => (state.openstudyList = openstudyList),
-    SET_IS_STUDY_MEMBER: (state,value) => (state.isStudyMember = value),
+
+    SET_OPENSTUDY_LIST: (state, openstudyList) =>
+      (state.openstudyList = openstudyList),
+    SET_IS_STUDY_MEMBER: (state, value) => (state.isStudyMember = value),
   },
 
   actions: {
@@ -88,34 +90,32 @@ export default {
       commit("SET_JOIN_LIST", joinArray);
     },
 
-    async saveOpenList({commit, getters}){
-      console.log('액시오스전')
+    async saveOpenList({ commit, getters }) {
+      console.log("액시오스전");
       await axios({
         url: rest.study.open_study(),
-        method:"get",
-        headers: getters.authHeader,
-      })
-      .then((res) => {
-        console.log(res)
-        commit("SET_OPENSTUDY_LIST", res.data)
-      })
-      .catch((err)=>{
-        console.log(err)
-      })
-    },
-
-
-
-    select_one({commit,getters},id){
-      console.log('진입 ')
-      axios({
-        url: rest.study.study_search(id),
         method: "get",
         headers: getters.authHeader,
       })
         .then((res) => {
-          commit("SET_SELECTED_STUDY", res.data)});
-          console.log(getters.selectedStudy)
+          console.log(res);
+          commit("SET_OPENSTUDY_LIST", res.data);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+
+    select_one({ commit, getters }, id) {
+      console.log("진입 ");
+      axios({
+        url: rest.study.study_search(id),
+        method: "get",
+        headers: getters.authHeader,
+      }).then((res) => {
+        commit("SET_SELECTED_STUDY", res.data);
+      });
+      console.log(getters.selectedStudy);
     },
 
     async selectStudy({ commit, getters, dispatch }, id) {
@@ -153,7 +153,7 @@ export default {
     },
 
     createStudy({ getters, dispatch }, credential) {
-      console.log("스터디 생성 요청",credential)
+      console.log("스터디 생성 요청", credential);
       axios({
         url: rest.study.study_create(),
         method: "post",
@@ -201,7 +201,7 @@ export default {
       }
     },
     myStudyList({ getters, commit }) {
-      if(getters.isLoggedIn){
+      if (getters.isLoggedIn) {
         axios({
           url: rest.study.my_study_list(),
           method: "get",
@@ -256,10 +256,11 @@ export default {
         headers: getters.authHeader,
       })
         .then((res) => {
-          alert('가입신청 완료')
-        console.log(res)})
+          alert("가입신청 완료");
+          console.log(res);
+        })
         .catch((err) => {
-          alert('이미 가입되거나 가입신청한 스터디입니다.')
+          alert("이미 가입되거나 가입신청한 스터디입니다.");
           console.log(err);
         });
     },
@@ -276,7 +277,7 @@ export default {
         })
           .then((res) => {
             dispatch("joinList");
-            dispatch("saveStudyMemberList",credential.study_pk)
+            dispatch("saveStudyMemberList", credential.study_pk);
           })
           .catch((err) => {
             console.log(err);
@@ -300,8 +301,8 @@ export default {
     },
 
     dropOutStudy({ getters }, user_pk) {
-      console.log(user_pk)
-      console.log(getters.selectedStudy.id)
+      console.log(user_pk);
+      console.log(getters.selectedStudy.id);
       axios({
         url: rest.study.study_member_out(),
         method: "delete",
@@ -310,12 +311,82 @@ export default {
           study_pk: getters.selectedStudy.id,
           user_pk: user_pk,
         },
-      })
+      });
       // .then((res) => {
       //   router.push({ name: "Home" });
       // })
       // .catch((err)=>console.log("에러내용:",err))
     },
+
+    // 캘린더
+    async selectEvent({ commit, getters }, id) {
+      await axios({
+        url: rest.calendar.select_calendar(id),
+        method: "get",
+        headers: getters.authHeader,
+      })
+        .then((res) => {
+          console.log(res.data);
+          commit("SET_EVENT", res.data);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+
+    async createCalendar({ commit, getters }, calendar) {
+      await axios({
+        url: rest.calendar.create_calendar(),
+        method: "post",
+        headers: getters.authHeader,
+        data: calendar,
+      })
+        .then((res) => {
+          axios({
+            url: rest.calendar.calendar_list(calendar.study_pk),
+            method: "get",
+            headers: getters.authHeader,
+          })
+            .then((res) => {
+              commit("SET_EVENTS", res.data);
+            })
+
+            .catch((err) => {
+              console.log(err);
+            });
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+
+    async deleteCalendar({ commit, getters }, event) {
+      await axios({
+        url: rest.calendar.delete_calendar(),
+        method: "delete",
+        headers: { Authorization: getters.token, id: event.id },
+      })
+        .then((res) => {
+          axios({
+            url: rest.calendar.calendar_list(event.study_pk),
+            method: "get",
+            headers: getters.authHeader,
+          })
+            .then((res) => {
+              console.log(2, res.data);
+              commit("SET_EVENTS", res.data);
+            })
+
+            .catch((err) => {
+              console.log(err);
+            });
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+
+    // 컴파일러
     async compile({ dispatch }, code) {
       console.log(code.script);
       // const temp = code.language.toLowerCase();
@@ -354,7 +425,7 @@ export default {
     },
 
     findStudyManager({ getters, commit }) {
-      let memberCheck = false
+      let memberCheck = false;
       getters.studyMemberList.forEach((member) => {
         if (member.authority == 5) {
           commit("SET_STUDY_MANAGER", {
@@ -362,12 +433,15 @@ export default {
             name: member.user_id,
           });
         }
-        if (member.user_pk == getters.loginUser.id){
-          memberCheck = true
+        if (member.user_pk == getters.loginUser.id) {
+          memberCheck = true;
         }
-      })
-      if (memberCheck){commit("SET_IS_STUDY_MEMBER",true)}
-      else{commit("SET_IS_STUDY_MEMBER",false)}
+      });
+      if (memberCheck) {
+        commit("SET_IS_STUDY_MEMBER", true);
+      } else {
+        commit("SET_IS_STUDY_MEMBER", false);
+      }
     },
   },
 };
