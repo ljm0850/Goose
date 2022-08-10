56f460d80be9b4700c018665dcbc8ebd45cb2b83
Ingyeon <qwe0274@naver.com>
2022-08-10 15:11:12 +0900

    feat: 댓글 페이지네이션 기초작업

--- frontend-new/goose/src/views/article/replyList.vue
+++ frontend-new/goose/src/views/article/replyList.vue
@@ -1,18 +1,26 @@
 <template>
  <h4>댓글 목록</h4>
  <replyListItem/>
+ <reply-nation></reply-nation>
  <reply-form/> 
 </template>
 
 <script>
+import ReplyNation from './pagenation/replyNation.vue'
 import replyForm from './replyForm.vue'
 import replyListItem from './replyListItem.vue'
+import {  useStore  } from 'vuex'
+import {  computed  } from 'vue'
+
 export default {
     components: {
         replyForm,
         replyListItem,
+        ReplyNation,
     },
-}
+    setup(){
+        }
+    }
 </script>
 
 <style>
