a9a7f994141fc75135dd4471826650adc1ff5752
leejaemin <dlwoals0850@gmail.com>
2022-08-11 15:59:49 +0900

    FE join accept &

--- frontend/src/components/StudyPage/studyJoinListItem.vue
+++ frontend/src/components/StudyPage/studyJoinListItem.vue
@@ -30,11 +30,11 @@ export default {
     const joinAccept = ()=>{
       store.dispatch('joinAgree',credential)
       // 신청 확인 후 화면 그대로 유지되는걸해결 위해 임시로 새로고침 넣음
-      router.go()
+      // router.go()
     }
     const joinRefuse = ()=>{
       store.dispatch('joinRefuse',credential.id)
-      router.go()
+      // router.go()
     }
     return {joinAccept,joinRefuse}
   }
